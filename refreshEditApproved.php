4c156186e880ac513016fcc0e023725b724509b5
Toni Hermoso Pulido <toniher@cau.cat>
2013-12-04 01:06:15 +0100

    fix approved rev

--- refreshEditApproved.php
+++ refreshEditApproved.php
@@ -32,31 +32,31 @@ class RefreshEdit extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = "Refresh link tables";
+		// Check Approval -> Last revision is approved revision
 		$this->addOption( 'approved', 'Only last approved' );
 		$this->addOption( 'm', 'Maximum replication lag', false, true );
-		$this->addArg( 'start', 'Page_id to start from, default 1', false );
-		$this->addArg( 'old', 'Handle only pages touched at most n miliseconds ago', false );
+		$this->addOption( 'start', 'Page_id to start from, default 1', false, true );
+		// Last modifications. Remove new-only
+		$this->addOption( 'old', 'Handle only pages touched at most n minutes', false, true );
 		$this->addOption( 'namespace', 'Namespace number, default all', false, true );
-		$this->addOption( 'category', 'Category name, default none', false, true );
 		$this->addOption( 'rewrite', 'Rewriting of the content, default 1', false, true );
 		$this->addOption( 'u', 'User to run the script', false, true );
 		$this->setBatchSize( 100 );
 		// Change options
-		// Check Approval -> Last revision is approved revision
-		
-		// Last modifications. Remove new-only
+
+
 	}
 
 	public function execute() {
 		$max = $this->getOption( 'm', 0 );
-		$start = $this->getArg( 0, 1 );
-		$old = $this->getArg( 1, 0 );
+		$start = $this->getOption( 'start', 1 );
+		$old = $this->getOption ( 'old', 0 );
 		$approved = $this->getOption( 'approved', true );
 		$ns = $this->getOption( 'namespace', -1 );
-		$category = $this->getOption( 'category', false );				
+		$category = $this->getOption( 'category', false );
 		$rewrite = $this->getOption( 'rewrite', 1 );
-		$u = $this->getOption( 'u', false );		
-
+		$u = $this->getOption( 'u', false );
+		
 		$this->doRefreshEdit( $start, $old, $approved, $max, $ns, $category, $rewrite, $u );
 	}
 
@@ -70,28 +70,22 @@ class RefreshEdit extends Maintenance {
 
 	private function doRefreshEdit( $start, $old = 0, $approved = true, $maxLag = false, $ns = -1, $category = false, $rewrite = 1, $u = false ) {
 
+		global $wgDBprefix;
+		
 		$reportingInterval = 100;
 		$dbr = wfGetDB( DB_SLAVE );
 		$start = intval( $start );
 		$old = intval( $old );
-		$ns_restrict = "page_namespace > -1";
+		$ns_restrict = $wgDBprefix."page.page_namespace > -1";
 		$tables = array('page');
-		$seltables = array( 'page_id' );
+		$seltables = array( $wgDBprefix.'page.page_id' );
 
 
 		// Need to do for NS
 		if ( $ns > -1 ) {
 			if ( is_numeric( $ns ) ) {
-				$ns_restrict = "page_namespace = $ns";
+				$ns_restrict = " ".$wgDBprefix."page.page_namespace = $ns";
 			}
-		}
-
-		// For categories
-		if ( $category ) {
-			array_push( $tables, "categorylinks" );
-			$category = mysql_real_escape_string( $category );
-			$category = str_replace(" ", "_", $category);
-			$ns_restrict.=" && cl_from = page_id && cl_to = '$category'";
 		}
 
 		// Approved latest
@@ -99,24 +93,26 @@ class RefreshEdit extends Maintenance {
 		
 			array_push( $tables, "approved_revs" );
 			// Only rev_id that are latest
-			$ns_restrict.=" && rev_id = page_latest";
+			$ns_restrict.=" AND ( ".$wgDBprefix."approved_revs.rev_id = ".$wgDBprefix."page.page_latest )";
 			
 		}
 		
-		
 		// Only if some values
 		if ( $old > 0 ) {
+			// From min to miliseconds
+			$old = $old*3600;
 			array_push( $tables, "revision" );
 			$timestamp = wfTimestamp( TS_MW ) - $old;
 			// We check page_touched
-			$ns_restrict.= " && page_latest = rev_id ";
-			$ns_restrict.= " && rev_timestamp > $timestamp ";
+			$ns_restrict.= " AND ( ".$wgDBprefix."revision.rev_id = ".$wgDBprefix."page.page_latest ) ";
+			$ns_restrict.= " AND ( ".$wgDBprefix."revision.rev_timestamp > $timestamp ) ";
+
 		}
 
 		$res = $dbr->select( $tables,
 			$seltables,
 			array(
-				"page_id >= $start" ,
+				$wgDBprefix."page.page_id >= $start" ,
 				$ns_restrict ),
 			__METHOD__
 		);
